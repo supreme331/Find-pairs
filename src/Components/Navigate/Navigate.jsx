c6982059982b892bddd67583cb4b74cc30045806
Vasiliy Kabanov <supreme331@yandex.ru>
2022-09-05 22:38:53 +0300

    add adaptive

--- src/Components/Navigate/Navigate.jsx
+++ src/Components/Navigate/Navigate.jsx
@@ -5,7 +5,7 @@ export const NavigateButton = ({to, title, LeftImage, RightImage, callBack}) => 
     return <NavLink onClick={callBack ? () => callBack() : () => {
     }} className={styles.root} to={to}>
         {LeftImage ? <img className={styles.leftImage} src={LeftImage} alt=""/> : null}
-        <div>{title}</div>
+        {title}
         {RightImage ? <img className={styles.image} src={RightImage} alt=""/> : null}
     </NavLink>
 }
